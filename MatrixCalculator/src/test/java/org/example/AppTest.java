d7d18f5bf7e99ee523e4289dc0c44a8f0e0cbaae
Gregory <grisha101202@yandex.ru>
2022-09-25 17:27:19 +0300

    add setUp method in AppTest Class

--- MatrixCalculator/src/test/java/org/example/AppTest.java
+++ MatrixCalculator/src/test/java/org/example/AppTest.java
@@ -11,10 +11,14 @@ public class AppTest {
     private Matrix matrix1;
     private Matrix matrix2;
     private Matrix matrix3;
+    private Matrix matrix4;
 
     @BeforeEach
     void setUp() { // Create object before compilation
-
+        matrix1 = new Matrix(new double[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        matrix2 = new Matrix(new double[][]{{11, 12, 13}, {2, 3, 1}, {18, 42,0}});
+        matrix3 = new Matrix(new double[][]{{1, 5, 9}, {7, 3, 2}});
+        matrix4 = new Matrix(new double[][]{{1, 0, 0}, {0, 1, 0}, {0, 0,1}});
     }
 
 }
