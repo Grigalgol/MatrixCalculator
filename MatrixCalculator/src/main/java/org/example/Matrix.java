39e2894011170126eaa22253ee60c1fe1453465b
Gregory <grisha101202@yandex.ru>
2022-09-25 16:45:15 +0300

    add class Matrix

---
+++ MatrixCalculator/src/main/java/org/example/Matrix.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Matrix {
+
+    private int nrows;
+    private int ncols;
+    private double[][] data;
+
+    public Matrix(double[][] dat) {
+        this.data = dat;
+        this.nrows = dat.length;
+        this.ncols = dat[0].length;
+    }
+
+    public Matrix(int nrow, int ncol) {
+        this.nrows = nrow;
+        this.ncols = ncol;
+        data = new double[nrow][ncol];
+    }
+}
