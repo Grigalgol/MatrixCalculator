9fc58c26e78ef704fde72edd076d067cbfcaed71
Gregory <grisha101202@yandex.ru>
2022-09-25 17:01:26 +0300

    add nessesary methods in App Class

--- MatrixCalculator/src/main/java/org/example/App.java
+++ MatrixCalculator/src/main/java/org/example/App.java
@@ -10,4 +10,13 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static Matrix addition(Matrix matrix1, Matrix matrix2){return null;}
+    public static Matrix subtraction(Matrix matrix1, Matrix matrix2){return null;}
+    public static Matrix multiply(Matrix matrix1, Matrix matrix2){return null;}
+    public static Matrix transpose(Matrix matrix) {return null;}
+    public static double determinant(Matrix matrix){return 0;}
+    public static Matrix createSubMatrix(Matrix matrix, int excluding_row, int excluding_col) {return null;}
+    public static Matrix inverse(Matrix matrix){return null;}
+    public static Matrix cofactor(Matrix matrix){return null;}
 }
